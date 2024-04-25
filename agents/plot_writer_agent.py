7643a81326eb4f6e38f09f22ac0fc928c4a93e86
Liss McCabe <liss@eristiccode.com>
2024-04-25 09:19:19 -0700

    add plot writer agent

--- agents/plot_writer_agent.py
+++ agents/plot_writer_agent.py
@@ -1,22 +1,90 @@
-from typing import Annotated, List
-from sqlalchemy import select
-from db import session
+import json
 
-from agent_superclass import AgentSuperclass
-from models import Campaign, Game, Player
+from typing import Annotated, List, Dict, TYPE_CHECKING
+
+from agents.functions.queries import find_game, find_player, find_character_sheets, find_campaign, update_campaign_story
+
+
+if TYPE_CHECKING:
+    from models.player import Player
+    from models.game import Game
+    from models.character_sheet import CharacterSheet
+    from models.background import Background
+    from models.race import Race
+
+from agents.agent_superclass import AgentSuperclass
+
 
 STARTING_GAME_ID = 1
 
 
 def get_game_context(
-    game_id: Annotated[int, "The id of the game to manage."],
-    player_id: Annotated[int, "The id of the player to manage."],
-) -> object:
-    stmt = select(Game.game_length__sessions, Game.tone, Game.setting).where(
-        Game.id == game_id
-    )
-    game = session.execute(stmt).scalars().first()
-    return game
+    game_id: Annotated[int, "The id of the game being played."],
+    player_id: Annotated[int, "The id of the current player."],
+    player_character_ids: Annotated[List[int], "The id of the character sheet the player is using."],
+) -> str:
+    game = find_game(game_id)
+    player = find_player(player_id)
+    pcs = find_character_sheets(player_character_ids)
+
+    pcs_list = []
+    for pc in pcs:
+        class_levels = pc.class_levels
+        class_names = list(set([cls["name"] for cls in class_levels]))
+        pcs_list.append({
+            "name": pc.name,
+            "class_names": class_names,
+            "background": pc.background.name,
+            "race": pc.race.name,
+            "alignment": pc.alignment,
+            "backstory": pc.backstory
+        })
+
+    game_context = f"""
+    Game:\n
+    Game Length: {game.game_length__sessions} session(s)
+    Session Length: {game.session_length__responses}
+    response(s)\nTone: {game.tone}\n
+    Setting: {game.setting}\n
+    \n
+    Player Veils (topics to do only off-screen): {player.veils}\n
+    Player Lines (topics to never do): {player.lines}\n
+    Player Loves (topics to include liberally): {player.loves}\n
+    Player Likes (topics to include judiciously): {player.likes}\n
+    \n
+    Player Characters:\n
+    {pcs_list}
+    """
+
+    return game_context
+
+def update_story(
+    game_id: Annotated[int, "The id of the game being played."],
+    summary: Annotated[str, "1-2 sentence summary of the plot"],
+    primary_goal: Annotated[str, "The primary goal of the game. When the player achieves this goal, the game will conclude."],
+    secondary_goals: Annotated[List[str], "The secondary goals of the game. Achieving these goals will help the player achieve the primary goal but are not strictly necessary. These can be adjusted, added to, or removed as the game progresses."],
+    steps: Annotated[Dict[str, List[str]], "The steps the player can take to achieve each goal. These can be adjusted, added to, or removed as the game progresses. Each step should be a clear, actionable task that the player can complete. Each goal should have 2 to 4 steps."],
+    obstacles: Annotated[List[str], "The obstacles the players will face. while trying to achieve the goals."],
+    setting_notes: Annotated[str, "Any notes on the setting that will be relevant to describing scenes or characterizing NPCs."],
+    key_friendlies: Annotated[List[str], "The key friendly characters in the game with an eye to their funciton in the plot. This will be given to the NPC creator to flesh out"],
+    key_antagonists: Annotated[List[str], "The key antagonists in the game with an eye to their function in the plot. This will be given to the NPC creator to flesh out"]
+) -> None:
+    story = json.dumps({
+        "summary": summary,
+        "primary_goal": primary_goal,
+        "secondary_goals": secondary_goals,
+        "steps": steps,
+        "obstacles": obstacles,
+        "setting_notes": setting_notes,
+        "key_friendlies": key_friendlies,
+        "key_antagonists": key_antagonists
+    })
+
+    update_campaign_story(game_id, story)
+
+def get_campaign_story(game_id: Annotated[int, "The id of the game being played."]) -> Dict[str, str]:
+    campaign = find_campaign(game_id)
+    return campaign.story
 
 
 class PlotWriterAgent(AgentSuperclass):
@@ -54,5 +122,15 @@ class PlotWriterAgent(AgentSuperclass):
                 "name": "get_game_context",
                 "description": "Read the game context from the database.",
                 "callable_function": get_game_context,
-            }
+            },
+            {
+                "name": "update_story",
+                "description": "Update the story in the database.",
+                "callable_function": update_story,
+            },
+            {
+                "name": "get_campaign_story",
+                "description": "Read the campaign story from the database.",
+                "callable_function": get_campaign_story,
+            },
         ]
